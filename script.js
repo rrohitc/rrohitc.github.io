527565eb32f81f05bbc3f801b121200d11962fd0
rrohitc <rohitchavan8401@gmail.com>
2024-02-24 16:09:26 +0530

    mobile adjustments
    mobile adjustments

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-let menu = document.querySelector('#menu-btn');
+let menu = document.querySelector('.navbar-toggler');
 let navbar = document.querySelector('.header .nav');
 let header = document.querySelector('.header');
+let icon = document.querySelector('.navbar-toggler .span .bi');
     
 menu.onclick = () =>{
-   menu.classList.toggle('bi-x');
+   // menu.classList.toggle('bi-x');
+   // menu.classList.toggle('bi-x');
    navbar.classList.toggle('active');
+   
 }
 
 window.onscroll = () =>{
-   menu.classList.remove('bi-x');
+   // menu.classList.remove('bi-x');
    navbar.classList.remove('active');
 
   if(window.scrollY > 0){
      header.classList.add('active');
   }else{
-     header.classList.remove('active');      
+     header.classList.remove('active');
+
 }
+}
+
+if(!navbar.classList.contains('active')){
+   menu.classList.remove('bi-x');
 }
